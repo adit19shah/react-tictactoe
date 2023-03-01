8007f866850a5b14ee03cdf02b638d46befce02d
Adit Shah <sahiten@tekion.com>
2023-03-01 17:05:48 +0530

    Add toggle switch to toggle the order of moves

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,12 +1,14 @@
 import Board from "./Board";
 import { useState } from "react";
+import ReactSwitch from "react-switch";
 
 export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   //Array(9).fill(null) : creates an array with nine elements and sets each of them to null
   const [currentMove, setCurrentMove] = useState(0);
   const currentSquares = history[currentMove];
-  const xIsNext = currentMove % 2 === 0;
+  const xIsNext = (currentMove & 1) === 0;
+  const [checked, setChecked] = useState(true);         //For toggle switch
 
   function handlePlay(nextSquares) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
@@ -16,6 +18,22 @@ export default function Game() {
 
   function jumpTo(nextMove) {
     setCurrentMove(nextMove);
+  }
+
+  function ToggleSwitch() {
+  
+    const handleChange = (val) => {
+      setChecked(val);
+      console.log("Clicked");
+    };
+  
+    return (
+      <div className="app" style={{ textAlign: "center" }}>
+        <h4>Toggle Switch to toggle the order</h4>
+        <ReactSwitch checked={checked} onChange={handleChange} />
+        
+      </div>
+    );
   }
 
   const moves = history.map((squares, move) => {
@@ -36,7 +54,15 @@ export default function Game() {
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
-        <ol>{moves}</ol>
+        <ToggleSwitch />
+        {
+            function(){
+                if(checked)
+                    return <ol>{moves}</ol>
+                else
+                    return <ol>{moves.reverse()}</ol>
+            }()
+        }
       </div>
     </div>
   );
